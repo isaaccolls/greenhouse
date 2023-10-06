41c6d8c7fc205f7552383c0bd7773df42f747e35
Isaac Colls <colls_isaac@hotmail.com>
2023-10-06 14:41:10 -0300

    feat: update aux scripts

--- off.py
+++ off.py
@@ -1,27 +1,25 @@
 import RPi.GPIO as GPIO
 import time
 
-led_panel = 14
-fan_cooler = 15
-status = 2
+# relay pinout
+relay_grass = 14
+relay_pine = 15
+relay_light = 2
+
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(led_panel, GPIO.OUT)
-GPIO.setup(fan_cooler, GPIO.OUT)
-GPIO.setup(status, GPIO.OUT)
+GPIO.setup(relay_grass, GPIO.OUT)
+GPIO.setup(relay_pine, GPIO.OUT)
+GPIO.setup(relay_light, GPIO.OUT)
 
 
 def off_everything():
-    print("off led_panel")
-    GPIO.output(led_panel, False)
-    print("off fan_cooler")
-    GPIO.output(fan_cooler, False)
-    print("off status")
-    GPIO.output(status, False)
-    time.sleep(1)
-    print("bye")
+    print("off_everything")
+    GPIO.output(relay_grass, False)
+    GPIO.output(relay_pine, False)
+    GPIO.output(relay_light, False)
 
 
 if __name__ == '__main__':
--- on.py
+++ on.py
@@ -1,25 +1,24 @@
 import RPi.GPIO as GPIO
 import time
 
-# relay_grass = 14
-# relay_pine = 15
-# relay_light = 2
-
-led_panel = 14
-fan_cooler = 15
-status = 2
+# relay pinout
+relay_grass = 14
+relay_pine = 15
+relay_light = 2
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(led_panel, GPIO.OUT)
-GPIO.setup(fan_cooler, GPIO.OUT)
-GPIO.setup(status, GPIO.OUT)
+GPIO.setup(relay_grass, GPIO.OUT)
+GPIO.setup(relay_pine, GPIO.OUT)
+GPIO.setup(relay_light, GPIO.OUT)
 
 
 def on_everything():
-    print("on led_panel")
-    GPIO.output(fan_cooler, True)
+    print("on_everything")
+    GPIO.output(relay_grass, True)
+    GPIO.output(relay_pine, True)
+    GPIO.output(relay_light, True)
 
 
 if __name__ == '__main__':
