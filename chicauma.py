d117010228b268b6358af5b952c38acd21e5872b
Isaac Colls <colls_isaac@hotmail.com>
2023-12-10 10:01:10 -0300

    feat: update time

--- chicauma.py
+++ chicauma.py
@@ -28,7 +28,7 @@ def relay_action(relay, action):
 
 def check_grass(weekday, hour, minute):
     print('-- check_grass')
-    if (hour == 10 or hour == 22) and (minute >= 0 and minute <= 10):
+    if (hour == 10 or hour == 22) and (minute >= 0 and minute <= 7):
         relay_action(relay_grass, False)
     else:
         relay_action(relay_grass, True)
@@ -36,7 +36,7 @@ def check_grass(weekday, hour, minute):
 
 def check_pine(weekday, hour, minute):
     print('-- check_pine')
-    if (weekday == 0 or weekday == 2 or weekday == 4) and hour == 7 and (minute >= 0 and minute <= 30):
+    if (weekday == 0 or weekday == 2 or weekday == 4) and hour == 8 and (minute >= 0 and minute <= 30):
         relay_action(relay_pine, False)
     else:
         relay_action(relay_pine, True)
